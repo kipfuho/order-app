24d445a8b72df94687f8cecbd87e03dbe160e12d
kipfuho <ctcakip@gmail.com>
2025-03-12 09:44:58 +0700

    refactor: service

--- server/src/auth/controllers/user.controller.js
+++ server/src/auth/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const httpStatus = require('http-status');
-const pick = require('../utils/pick');
-const ApiError = require('../utils/ApiError');
-const catchAsync = require('../utils/catchAsync');
+const pick = require('../../utils/pick');
+const ApiError = require('../../utils/ApiError');
+const catchAsync = require('../../utils/catchAsync');
 const { userService } = require('../services');
 
 const createUser = catchAsync(async (req, res) => {
