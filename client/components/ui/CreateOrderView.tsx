b02e2863c354c612c10bb1be4d5430de005dfebf
kipfuho <ctcakip@gmail.com>
2025-06-30 22:16:47 +0700

    fix: locale

--- client/components/ui/CreateOrderView.tsx
+++ client/components/ui/CreateOrderView.tsx
@@ -228,7 +228,7 @@ export default function CreateOrder({
                     alignSelf: "center",
                   }}
                 >
-                  {dishType}
+                  {t(dishType)}
                 </Button>
               ))}
             </ScrollView>
